811273b808eb45f1dd490fddcef46f1f9161c0a0
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2026-01-30 14:24:49 +0900

    Update documentation and sample notebook
    - Update CLAUDE.md with current architecture and features
    - Update samples/Basic.jl with additional interactive widgets

--- samples/Basic.jl
+++ samples/Basic.jl
@@ -12,14 +12,8 @@ begin
     using PlutoUI
 end
 
-# ╔═╡ 1a453479-8e69-4c0e-8d2f-4578d5b74353
-@bind a Slider(-1:0.01:1)
-
-# ╔═╡ 636f16e1-b458-4d38-abe1-92f24d7173da
-println(z)
-
-# ╔═╡ 63c880b2-5bba-43e2-81e2-efa880aabe6c
-@bind f Select([sin, cos, tan])
+# ╔═╡ cfec83b9-4cb3-4f5d-b6ef-799076b66485
+plot(x, y)
 
 # ╔═╡ de2172e1-0c9c-44bb-bb14-f5935004e5cd
 begin
@@ -27,8 +21,11 @@ begin
     y = @. f(x - a)
 end
 
-# ╔═╡ cfec83b9-4cb3-4f5d-b6ef-799076b66485
-plot(x, y)
+# ╔═╡ 1a453479-8e69-4c0e-8d2f-4578d5b74353
+@bind a Slider(-1:0.01:1)
+
+# ╔═╡ 63c880b2-5bba-43e2-81e2-efa880aabe6c
+@bind f Select([sin, cos, tan])
 
 # ╔═╡ 08311720-0b8b-4c29-9ec3-4593231e3ad3
 testimage("mand")
@@ -38,10 +35,9 @@ testimage("mand")
 
 # ╔═╡ Cell order:
 # ╠═2d9e9e13-f659-4fd3-b318-6bd3016fbc38
-# ╠═1a453479-8e69-4c0e-8d2f-4578d5b74353
-# ╠═636f16e1-b458-4d38-abe1-92f24d7173da
-# ╠═63c880b2-5bba-43e2-81e2-efa880aabe6c
-# ╠═de2172e1-0c9c-44bb-bb14-f5935004e5cd
 # ╠═cfec83b9-4cb3-4f5d-b6ef-799076b66485
+# ╠═de2172e1-0c9c-44bb-bb14-f5935004e5cd
+# ╠═1a453479-8e69-4c0e-8d2f-4578d5b74353
+# ╠═63c880b2-5bba-43e2-81e2-efa880aabe6c
 # ╠═08311720-0b8b-4c29-9ec3-4593231e3ad3
 # ╠═f64c9a4d-4ad8-4f0c-9641-9e9a1613ab0a
