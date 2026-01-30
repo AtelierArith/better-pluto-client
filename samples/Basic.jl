01b3505f52ce42ead5241759e56944f839b79564
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2026-01-30 14:03:52 +0900

    Update sample notebook with additional interactive widgets
    - Add Slider with continuous range for smoother interaction
    - Add Select widget to choose different functions
    - Update code to use selected function with offset parameter

--- samples/Basic.jl
+++ samples/Basic.jl
@@ -13,15 +13,18 @@ begin
 end
 
 # ╔═╡ 1a453479-8e69-4c0e-8d2f-4578d5b74353
-@bind z Slider([1, 2, 3, 4, 5])
+@bind a Slider(-1:0.01:1)
 
 # ╔═╡ 636f16e1-b458-4d38-abe1-92f24d7173da
 println(z)
 
+# ╔═╡ 63c880b2-5bba-43e2-81e2-efa880aabe6c
+@bind f Select([sin, cos, tan])
+
 # ╔═╡ de2172e1-0c9c-44bb-bb14-f5935004e5cd
 begin
     x = -pi:0.01:pi
-    y = sin.(x)
+    y = @. f(x - a)
 end
 
 # ╔═╡ cfec83b9-4cb3-4f5d-b6ef-799076b66485
@@ -37,6 +40,7 @@ testimage("mand")
 # ╠═2d9e9e13-f659-4fd3-b318-6bd3016fbc38
 # ╠═1a453479-8e69-4c0e-8d2f-4578d5b74353
 # ╠═636f16e1-b458-4d38-abe1-92f24d7173da
+# ╠═63c880b2-5bba-43e2-81e2-efa880aabe6c
 # ╠═de2172e1-0c9c-44bb-bb14-f5935004e5cd
 # ╠═cfec83b9-4cb3-4f5d-b6ef-799076b66485
 # ╠═08311720-0b8b-4c29-9ec3-4593231e3ad3
